c818e03a230a16afa5659aec1766732c3494fff6
VishnuvardhanR <vishnuvardhan.u@ensurity.com>
2022-03-10 17:50:40 +0530

    added ui for students Data

--- Pages/Student/StudentModal.cshtml.cs
+++ Pages/Student/StudentModal.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RDLCReports.Contents;
+using RDLCReports.Data.StudentsTable;
 
 namespace RDLCReports.Pages.Student
 {
     public class StudentModalModel : PageModel
     {
-        public void OnGet()
+       
+        public List<GetAllDto> getAllStudents { get; set; }
+        private readonly IStudentAppService _studentAppService;
+        public StudentModalModel(IStudentAppService studentAppService)
         {
+            _studentAppService = studentAppService;
+        }
+        public async void OnGet()
+        {
+            var data = await _studentAppService.GetAllListAsync();
+            getAllStudents = data;
         }
     }
 }
--- Pages/Student/StudentModal.js
+++ Pages/Student/StudentModal.js
@@ -1,1 +1,84 @@
-﻿
+﻿//$(function () {
+//    var nFTSEOtagService = window.rDLCReports.student.student;
+
+//    var dataTable = $("#StudentsTable").DataTable(abp.libs.datatables.normalizeConfiguration({
+//        processing: true,
+//        serverSide: true,
+//        paging: true,
+//        searching: false,
+//        scrollX: true,
+//        autoWidth: false,
+//        scrollCollapse: true,
+//        order: [[1, "asc"]],
+//        ajax: abp.libs.datatables.createAjax(studentService.getAllList, getFilter),
+//        columnDefs: [
+//            //{
+//                //rowAction: {
+//                //    items:
+//                //        [
+//                            //{
+//                            //    text: l("Edit"),
+//                            //    visible: abp.auth.isGranted('Marketplace.NFTSEOtags.Edit'),
+//                            //    action: function (data) {
+//                            //        editModal.open({
+//                            //         id: data.record.nFTSEOtag.id
+//                            //         });
+//                            //    }
+//                            //},
+//                            //{
+//                            //    text: l("Delete"),
+//                            //    visible: abp.auth.isGranted('Marketplace.NFTSEOtags.Delete'),
+//                            //    confirmMessage: function () {
+//                            //        return l("DeleteConfirmationMessage");
+//                            //    },
+//                            //    action: function (data) {
+//                            //        nFTSEOtagService.delete(data.record.nftseOtag.id)
+//                            //            .then(function () {
+//                            //                abp.notify.info(l("SuccessfullyDeleted"));
+//                            //                dataTable.ajax.reload();
+//                            //            });
+//                            //    }
+//                            //}
+//            //            ]
+//            //    }
+//            //},
+//            //{
+//            //    data: "nftseOtag.tagName"
+//            //},
+//            //{ data: "nftseOtag.tagvalue" },
+//            //{
+//            //    render: function (data, text, row) {
+//            //        if (row.nftMaster != null) {
+//            //            return row.nftMaster.name;
+//            //        }
+//            //        else {
+//            //            return "-- NAN --";
+//            //        }
+
+//            //    }
+//            //},
+//            //{
+//            //    render: function (data, text, row) {
+//            //        if (row.category != null) {
+//            //            return row.category.name;
+//            //        }
+//            //        else {
+//            //            return "-- NAN --";
+//            //        }
+
+//            //    }
+//            //},
+//            //{
+//            //    render: function (data, text, row) {
+//            //        if (row.collection != null) {
+//            //            return row.collection.name;
+//            //        }
+//            //        else {
+//            //            return "-- NAN --";
+//            //        }
+
+//            //    }
+//            //}
+//        ]
+//    }));
+//});
